63f0654b5d9b548f2883758926ce98f1c7d864de
justinait <justiiturraspe@gmail.com>
2024-05-24 20:38:32 -0300

    pc

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -15,19 +15,22 @@ function Home() {
             <p className='ourFocusText'>We aim to provide our clients with a unique and exclusive service, offering 
                 luxury cars at your convenience. <br /> <strong>Whenever and wherever you need them.</strong></p>
         </div>
-        <div className='homeImageContainer'>
-            <div className='overlay'></div>
-            <p className='homeImagePhrase'>Let's get you on the road.<br /> Expertise. Commitment. Value.</p>
-            <img src={image3} alt="" className='homeServicesImage'/>
-        </div>
+        <div className='homeDiv'>
+
+            <div className='homeImageContainer'>
+                <div className='overlay'></div>
+                <p className='homeImagePhrase'>Let's get you on the road.<br /> Expertise. Commitment. Value.</p>
+                <img src={image3} alt="" className='homeServicesImage'/>
+            </div>
             <p className='ourFocus ourServices'>OUR SERVICES</p>
-        <div className='homeImageContainer'>
-            <div className='overlay'></div>
-            <img className='homeServicesImage' src={image4} alt="" />
-            <div className='homeImagePhrase homeImagePhraseTablet'>
-                <p className='homeImageP'>Unforgettable Unique Experience. </p>
-                <p className='homeImageP'>Reservations at Nightclubs and Restaurants.</p>
-                <p className='homeImageP'>Logistics for Easy Parking in High-Demand Areas.</p>
+            <div className='homeImageContainer'>
+                <div className='overlay'></div>
+                <img className='homeServicesImage' src={image4} alt="" />
+                <div className='homeImagePhrase homeImagePhraseTablet'>
+                    <p className='homeImageP'>Unforgettable Unique Experience. </p>
+                    <p className='homeImageP'>Reservations at Nightclubs and Restaurants.</p>
+                    <p className='homeImageP'>Logistics for Easy Parking in High-Demand Areas.</p>
+                </div>
             </div>
         </div>
     </div>
