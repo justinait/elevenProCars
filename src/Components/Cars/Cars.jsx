db9fbd3c2d607e8286928a14bac3c9759d0a5ac2
justinait <justiiturraspe@gmail.com>
2024-05-24 18:06:44 -0300

    image

--- src/Components/Cars/Cars.jsx
+++ src/Components/Cars/Cars.jsx
@@ -4,6 +4,12 @@ function Cars() {
   return (
     <div>
         <h2>Our Cars</h2>
+        <div>
+
+            <p>Drive the car you want</p>
+            <p>Expertise. Commitment. Value.</p>
+            <img src={image} alt="" className='homeImage'/>
+        </div>
     </div>
   )
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
+import image from '/cars/maclaren naranja (1) cut.jpg'
+import image2 from '/cars/tres autos (2).jpg'
+import image3 from '/cars/mercedes camioneta blanca (2).jpg'
+
+import './Home.css'
 
 function Home() {
   return (
     <div>
-        <p>WHAT WE</p>
-        <p>OFFER</p>
-        
-        <p>Drive the car you want</p>
-        <p>Let's get you on the road</p>
-        <p>Expertise. Commitment. Value.</p>
+        <div className='homeContainerDiv'>
+            <p className='ourFocus'>OUR FOCUS</p>
+            <p className='ourFocusText'>We aim to provide our clients with a unique and exclusive service, offering 
+                luxury cars at your convenience. <br /> <strong>Whenever and wherever you need them.</strong></p>
+        </div>
+        <div className='homeImageContainer'>
+            <div className='overlay'></div>
+            <p className='homeImagePhrase'>Let's get you on the road.<br /> Expertise. Commitment. Value.</p>
+            <img src={image3} alt="" className='homeServicesImage'/>
+        </div>
+        <div>
+            <p className='ourFocus ourServices'>OUR SERVICES</p>
+            <div>
+                <p className='ourFocusText'>
+                    Unforgettable Unique Experience. <br />
+                    Exclusive Car to Enjoy Mallorca's Coves and Beaches. <br />
+                    Reservations at Nightclubs and Restaurants. <br />
+                    Logistics for Easy Parking in High-Demand Areas.
+                </p>
+            </div>
+        </div>
     </div>
   )
 }
