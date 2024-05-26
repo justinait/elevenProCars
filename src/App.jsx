8256e0f2f558a1ecb384ef822f55a84cb8c27996
justinait <justiiturraspe@gmail.com>
2024-05-26 16:38:00 -0300

    vercel json

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Cars from './Components/Cars/Cars'
+import Footer from './Components/Footer/Footer'
 import Hero from './Components/Hero/Hero'
 import Home from './Components/Home/Home'
 import Links from './Components/Links/Links'
@@ -14,6 +15,7 @@ function App() {
       <Home />
       <Links />
       <Cars/>
+      {/* <Footer /> */}
     </>
   )
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return (
+    <div className='footerContainer'>
+        <div className='overlayHero'></div>
+        <h2>GET IN TOUCH</h2>
+    </div>
+  )
+}
+
+export default Footer
