81dbc8a0455d815ba966340e8ef89cbc2e7dec17
justinait <justiiturraspe@gmail.com>
2024-05-26 16:09:28 -0300

    ?

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 import image3 from '/cars/RangeRoverSportBlanca (2).jpg'
 // import image4 from '/cars/mercedes deportivo gris.jpg'
 import image4 from '/cars/VolanteMercedes3.jpg'
-
 import image from '/cars/maclarennaranja (1).jpg'
 import image5 from '/cars/2Backmercedesjeep (1).jpg'
 
