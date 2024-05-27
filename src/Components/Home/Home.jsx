a14956902c5b0e6dee44dd9ae26aea3f07f22b47
justinait <justiiturraspe@gmail.com>
2024-05-27 17:07:29 -0300

    about

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,11 +5,24 @@ import image3 from '/cars/hero.jpg'
 import image4 from '/cars/VolanteMercedes3.jpg'
 import image from '/cars/maclarennaranja (1).jpg'
 import image5 from '/cars/2Backmercedesjeep (1).jpg'
-
-
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import DownloadIcon from '@mui/icons-material/Download';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LoyaltyIcon from '@mui/icons-material/Loyalty';
 import './Home.css'
 
 function Home() {
+    const downloadPDF = () => {
+        const pdfPath = 'https://drive.google.com/file/d/19Drxs_hA-NHrFKU717mxexuMp6hvFPj6/view?usp=sharing';
+        const link = document.createElement('a');
+        link.href = pdfPath;
+        link.target = '_blank';
+        link.download = 'CaseStudyBenefit.pdf';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      };
   return (
     <div>
         <div className='homeContainerDiv'>
@@ -25,17 +38,58 @@ function Home() {
                 <img src={image3} alt="" className='homeServicesImage mobileOnly'/>
                 <img src={image5} alt="" className='homeServicesImage desktopOnly'/>
             </div>
-            <p className='ourFocus ourServices'>OUR SERVICES</p>
-            <div className='homeImageContainer'>
-                <div className='overlay desktopOnly'></div>
-                <img className='homeServicesImage mobileOnly' src={image4} alt="" />
-                <img className='homeServicesImage desktopOnly' src={image} alt="" />
-                <div className='homeImagePhrase homeImagePhraseTablet'>
+
+            {/* <p className='ourFocus ourServices'>OUR SERVICES</p> */}
+            <div className='aboutDiv'>
+                <div className='overlay darkerOverlay'></div>
+                <div className='aboutDivContainer'>
+                    <p className='ourFocus'>ABOUT US</p>
+                    <div className='aboutInfoContainer'>
+                        <div>
+                            <div>
+                                <p className='aboutUsTitles'><LocationOnIcon fontSize='small'/> Where?</p>
+                                <p>Airports, Hotels, or
+                                wherever you need in
+                                Palma de Mallorca, Spain
+                                </p>
+                            </div>
+                            <div>
+                                <p className='aboutUsTitles'><InstagramIcon fontSize='small'/>Instagram</p>
+                                <a style={{cursor: 'pointer', color: 'white' }} target='_blank' href='https://wa.link/qva9cx'>@elevenpro_cars</a>
+                            </div>
+                            
+                            <div>
+                                <p className='aboutUsTitles'><DownloadIcon fontSize='small'/>Catalog</p>
+                                <p onClick={downloadPDF} 
+                                style={{cursor: 'pointer', textDecoration: 'underline' }}> Download here</p>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <p className='aboutUsTitles'><CalendarMonthIcon fontSize='small'/>Reservations</p>
+                                <p> <a  style={{cursor: 'pointer', textDecoration: 'underline', color: 'white' }} target='_blank' href='https://wa.link/qva9cx'> click here</a> or <br />+34 634 18 70 73</p>
+                            </div>
+                            <div>
+                                <p className='aboutUsTitles'><LoyaltyIcon fontSize='small'/>Why us?</p>
+                                <ul> 
+                                    <li>Reservations at nightclubs and Restaurants.</li>
+                                    <li>Logistics for easy parking in high-demand areas</li>
+                                    <li>Unforgettable unique experience.</li>
+                                </ul>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+                {/* <img className='homeServicesImage mobileOnly' src={image4} alt="" />
+                <img className='homeServicesImage desktopOnly' src={image} alt="" /> */}
+                {/*<div className='homeImagePhrase homeImagePhraseTablet'>
                     <p className='homeImageP'>Unforgettable Unique Experience. </p>
                     <p className='homeImageP'>Reservations at Nightclubs and Restaurants.</p>
                     <p className='homeImageP'>Logistics for Easy Parking in High-Demand Areas.</p>
-                </div>
+                </div> */}
             </div>
+
         </div>
     </div>
   )
