c7e58dbe33aaa1913ceb75852012d542e92a3ec3
justinait <justiiturraspe@gmail.com>
2024-05-29 12:42:35 -0300

    new cars

--- src/Components/Cars/Cars.jsx
+++ src/Components/Cars/Cars.jsx
@@ -1,23 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import './Cars.css'
-import Mercedesg63 from '/cars/mercedes-benz-g63-amg (3).jpg'
-import Mercedesg63Back from '/cars/mercedes-benz-g63-amg (1).jpg'
-import RangeRoverSport from '/cars/RangeRoverSportBlancaCut (2).jpg'
-import RangeRoverSportBlack from '/cars/RangeRoverSportNegra (2).jpg'
-import RangeRoverSportSVG from '/cars/RangeRoverSportSVGBlanca (2).jpg'
-import mercedesCamioneta from '/cars/mercedesGL (1).jpg'
-import mercedesCamionetaBack from '/cars/mercedesGL (2).jpg'
-import negro from '/cars/negro.jpg'
-import mercedesS63 from '/cars/mercedesS63Amg.jpg'
-import mercedesGLC from '/cars/Glccoupeamglíneaamg.jpg'
-import mercedesGLE from '/cars/Mercedesgle.jpg'
-import mercedesGLCSUV from '/cars/MercedesGLCGris.jpg'
-import mercedesGLC220 from '/cars/mercedesGLCNegro (1).jpg'
-import jeepRubicon from '/cars/jeep (1).jpg'
-import GLBAzul from  '/cars/glb.jpg'
-import mercedesClaseA from '/cars/ClaseAMercedes2.jpg'
-import mercedesGLA from '/cars/mercedesGLA (2).jpg'
-import mercedesClaseC from '/cars/mercedesClassC3.jpg'
 
 function Cars() {
 
