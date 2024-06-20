5b26102f99b37579976f9ccc36d770f44d9f6108
justinait <justiiturraspe@gmail.com>
2024-06-20 10:38:00 -0300

    register validations

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -63,8 +63,10 @@ const Login = () => {
 
   return (
     <div className="container">
+      {/* <h2>Log in</h2> */}
       <form onSubmit={handleSubmit} className="form">
-        <div className="input">
+      <h3>Are you an affiliate from Eleven Pro Cars? <strong className="strongh3">LOG IN</strong> </h3>
+        <div className="">
           <input
             type="text"
             name="email"
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -15,35 +15,59 @@ const Register = () => {
     confirmPassword:"",
     instagram: ""
   })
+  const [showConfirmationMessage, setShowConfirmationMessage] = useState(false);
+  const [errors, setErrors] = useState({});
 
   const handleChange = (e) => {
     setUserCredentials({...userCredentials, [e.target.name]: e.target.value})
+    setErrors({ ...errors, [e.target.name]: "" });
   }
+  const validateForm = () => {
+    const newErrors = {};
+    if (!userCredentials.email) newErrors.email = "This field is required.";
+    if (!userCredentials.instagram) newErrors.instagram = "This field is required";
+    if (!userCredentials.password) newErrors.password = "Password is required.";
+    if (userCredentials.password.length < 6) newErrors.password = "Password must be at least 6 characters.";
+    if (userCredentials.password !== userCredentials.confirmPassword) newErrors.confirmPassword = "Las contraseñas no coinciden.";
+    return newErrors;
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setErrors({});
+    const validationErrors = validateForm();
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
     try {
       let res = await signUp(userCredentials);
-      if(res.user.uid){
-        await setDoc(doc(db, "users", res.user.uid), { 
+      if (res.user.uid) {
+        await setDoc(doc(db, "users", res.user.uid), {
           email: userCredentials.email,
           instagram: userCredentials.instagram,
           rol: "user",
-          isApproved: false 
-        } )
-        //cambié esto
+          isApproved: false
+        });
       }
-      navigate('/login');
+      setShowConfirmationMessage(true);
+      setTimeout(() => {
+        navigate('/login');
+      }, 5000);
     } catch (error) {
-      console.log(error);  
+      console.log(error);
     }
-  }
+  };
+
   const handleClickShowPassword = () => setShowPassword(!showPassword);
 
   return (
-    <div
-      className="container"
-    >
+    <div      className="container"    >
+      {showConfirmationMessage ? (
+        <div className="confirmationMessage">
+          The administrator will check your details and notify you shortly if your account is confirmed. Thank you. Eleven Pro Cars
+        </div>
+      ) : (
       <form onSubmit={handleSubmit}>
         <div className="form">
           <input
@@ -53,6 +77,7 @@ const Register = () => {
             className="input"
             onChange={handleChange}
           />
+          {errors.email && <p className="error">{errors.email}</p>}
           <input
             type="text"
             name="instagram"
@@ -60,6 +85,7 @@ const Register = () => {
             className="input"
             onChange={handleChange}
           />
+          {errors.instagram && <p className="error">{errors.instagram}</p>}
           <div className="passwordContainer registerPassword">
             <input
               type={showPassword ? "text" : "password"}
@@ -75,6 +101,7 @@ const Register = () => {
               {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
             </span>
           </div>
+          {errors.password && <p className="error">{errors.password}</p>}
           <div className="passwordContainer registerPassword">
             <input
               type={showPassword ? "text" : "password"}
@@ -90,6 +117,9 @@ const Register = () => {
               {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
             </span>
           </div>
+
+          {errors.confirmPassword && <p className="error">{errors.confirmPassword}</p>}
+
           <Link to="/login" className="notAccountYet">
             I already have an account
           </Link>
@@ -101,6 +131,7 @@ const Register = () => {
           </button>
         </div>
       </form>
+      )}
     </div>
   );
 };
