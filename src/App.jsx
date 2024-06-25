4cb4f11ecbc4617ec594972cf63980a7caa510a8
justinait <justiiturraspe@gmail.com>
2024-06-25 20:32:25 -0300

    opt fonts

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,8 @@ import DashboardAdminCRUD from './Components/DashboardAdmin/DashboardAdminCRUD'
 import { useEffect } from 'react'
 import RefCodeManager from './RefCodeManager'
 import { ReferenceContextProvider } from './context/ReferenceContext';
+import '@fontsource-variable/dm-sans';
+import '@fontsource/libre-caslon-text';
 
 function App() {
   
