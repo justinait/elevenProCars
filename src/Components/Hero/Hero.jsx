4b349e11a30082f06934b3d19c55b89190fc974b
justinait <justiiturraspe@gmail.com>
2024-06-27 11:35:13 -0300

    hero mobile

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import hero from '/cars/hero.jpg'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 import './Hero.css'
+import EastIcon from '@mui/icons-material/East';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
 function Hero() {
   return (
     <div className='heroContainer'>
       {/* <div className='overlayHero'></div> */}
-      {/* <p className='titleHero'>Mallorca Rental Luxury Cars Agency</p> */}
-      <p className='heroText'>Mallorca, Spain</p>
+      <p className='getStartedHero animate__animated animate__pulse'>Get started <ArrowForwardIcon/> </p>
+      <p className='heroText'> <LocationOnIcon fontSize='small'/> Mallorca, Spain</p>
     </div>
   )
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,37 +1,23 @@
 import React from 'react'
-// import image from '/cars/azul maclaren(1).jpg'
-import image3 from '/cars/hero.jpg'
-// import image4 from '/cars/mercedes deportivo gris.jpg'
-import image4 from '/cars/VolanteMercedes3.jpg'
-import image from '/cars/maclarennaranja (1).jpg'
-import image5 from '/cars/2Backmercedesjeep (1).jpg'
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import DownloadIcon from '@mui/icons-material/Download';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import LoyaltyIcon from '@mui/icons-material/Loyalty';
 import './Home.css'
 import Hero from '../Hero/Hero'
 import Starred from '../Starred/Starred'
-import OtherHousesIcon from '@mui/icons-material/OtherHouses';
-import BedIcon from '@mui/icons-material/Bed';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
 import Services from '../Services/Services'
 
 function Home() {
   return (
     <div>
-        <Hero/>
-        <div className='homeContainerDiv'>
-            <p className='ourFocus'>OUR FOCUS</p>
-            <p className='ourFocusText'>
-                We aim to provide our clients with a unique and exclusive service, offering 
-                luxury cars at your convenience. <br /> <strong>Whenever and wherever you need them.</strong>
-            </p>
-        </div>
-        
-        <Services />
-        <Starred />
+      <Hero/>
+      <div className='homeContainerDiv'>
+        <p className='ourFocus'>OUR FOCUS</p>
+        <p className='ourFocusText'>
+          We aim to provide our clients with a unique and exclusive service, offering 
+          luxury cars at your convenience. <br /> <strong>Whenever and wherever you need them.</strong>
+        </p>
+      </div>
+      
+      <Services />
+      <Starred />
 
     </div>
   )
