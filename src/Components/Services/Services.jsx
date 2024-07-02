7bd04990d10f8bdef84cfd4a3a38272fc4b61fbe
justinait <justiiturraspe@gmail.com>
2024-07-02 12:26:53 -0300

    full responsive

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,23 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
 import './Services.css'
 
 function Services() {
-    const settings = {
-      // dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      lazyLoad: 'ondemand',
-      arrows: false,
-      autoplay: true,
-      autoplaySpeed: 4000 
+  const [isDesktopScreen, setIsDesktopScreen] = useState(window.innerWidth >= 1200);
+  
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktopScreen(window.innerWidth >= 1200);
     };
     
-    const images = [
+    window.addEventListener('resize', handleResize);
+    
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const settings = {
+    // dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    lazyLoad: 'ondemand',
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000
+    };
+    
+    const images = !isDesktopScreen ? [
       {
         url: '/home/housemallorca3.jpg',
         text: 'We manage your stay in villas, chales, houses.',
@@ -39,7 +53,28 @@ function Services() {
         text: 'Unique experiences in the island.',
         className: 'blackText'
       },
-    ];
+    ] : [
+      {
+        url: '/home/house2.jpg',
+        text: 'We manage your stay in villas, chales, houses.',
+      },
+      {
+        url: '/home/aerop.jpg',
+        text: 'Private transport Mallorca. Airports, Hotels, or wherever you need.'
+      },
+      {
+        url: '/home/bar2.jpg',
+        text: 'Reservations at NightClubs and Restaurants.'
+      },
+      {
+        url: '/home/driver2.jpg',
+        text: 'Logistics for easy parking in high-demand areas.'
+      },
+      {
+        url: '/home/yacht.jpg',
+        text: 'Unique experiences in the island.'
+      },
+    ]
       
 
   return (
